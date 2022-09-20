b1dd622e18737f178e2e66747ad43df1b9cbce7a
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-09-19 22:41:43 -0400

    added dependencies and starter files

---
+++ client/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Route, Router, hashHistory, IndexRoute } from "react-router";
+
+import SongList from "./components/SongList";
+import App from "./components/App";
+import SongCreate from "./components/SongCreate";
+import "./style/style.css";
+
+const client = new ApolloClient({});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate}></Route>
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
