0f0e14148de9fcf6dcb3599af4dafbc81de89bab
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-09-25 09:25:21 -0400

    added like count

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class LyricList extends Component {
   onLyricLike(id) {
     console.log(id);
-    // this.props
-    //   .mutate({ variables: { id } })
-    //   .then(() => this.props.data.refetch());
+    this.props.mutate({ variables: { id } });
   }
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <div>
           <li key={id} className="collection-item">
             {content}
-            <i
-              className="like material-icons"
-              onClick={() => this.onLyricLike(id)}
-            >
-              thumb_up
-            </i>
+            <div className="vote-box">
+              <i
+                className="like material-icons"
+                onClick={() => this.onLyricLike(id)}
+              >
+                thumb_up
+              </i>
+              {likes}
+            </div>
           </li>
         </div>
       );
@@ -29,4 +32,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      content
+      likes
+    }
+  }
+`;
+export default graphql(mutation)(LyricList);
